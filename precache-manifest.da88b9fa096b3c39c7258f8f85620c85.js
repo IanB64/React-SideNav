14f56a7945e097ddd969a728a7912b2e6eca4452
Yilun Bai <yilunbai@gmail.com>
2020-06-03 21:42:16 -0400

    Updates

---
+++ precache-manifest.da88b9fa096b3c39c7258f8f85620c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb965a73d6ae5ff3eb2845b63f6b4097",
+    "url": "/React-SideNav/index.html"
+  },
+  {
+    "revision": "299b93ac7b01ad845299",
+    "url": "/React-SideNav/static/css/main.bffe6559.chunk.css"
+  },
+  {
+    "revision": "a65bcee5ff422c260530",
+    "url": "/React-SideNav/static/js/2.0b0f06d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-SideNav/static/js/2.0b0f06d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299b93ac7b01ad845299",
+    "url": "/React-SideNav/static/js/main.fe32f041.chunk.js"
+  },
+  {
+    "revision": "1c39653658db354292e5",
+    "url": "/React-SideNav/static/js/runtime-main.1f1a6c7c.js"
+  }
+]);
